c7598046c65e10fe79b5494f7b0567272c641d14
achaplin1 <85441727+achaplin1@users.noreply.github.com>
2025-06-17 20:28:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,43 +11,63 @@ const io = new Server(server, {
   cors: { origin: '*' }
 });
 
+const sampleQuestions = [
+  "Qui adore les chats ?",
+  "Qui choisirait toujours du bleu ?",
+  "Qui pourrait jouer dans un film d'action ?",
+];
+
 const rooms = {};
 
 io.on('connection', (socket) => {
   socket.on('joinRoom', ({ room, name }) => {
     socket.join(room);
-    if (!rooms[room]) rooms[room] = { players: [], answers: {}, index: 0 };
-    if (!rooms[room].players.includes(name)) rooms[room].players.push(name);
-    io.to(room).emit('updatePlayers', rooms[room].players);
+
+    if (!rooms[room]) {
+      rooms[room] = { players: [], answers: {}, index: 0 };
+    }
+
+    const roomData = rooms[room];
+
+    if (!roomData.players.includes(name)) {
+      roomData.players.push(name);
+    }
+
+    io.to(room).emit('updatePlayers', roomData.players);
   });
 
   socket.on('startGame', (room) => {
-    rooms[room].index = 0;
-    io.to(room).emit('nextQuestion', rooms[room].index);
+    const roomData = rooms[room];
+    if (!roomData) return;
+    roomData.index = 0;
+    roomData.answers = {};
+    io.to(room).emit('nextQuestion', roomData.index);
   });
 
   socket.on('answer', ({ room, questionIndex, player }) => {
-    const q = questionIndex;
-    if (!rooms[room].answers[q]) rooms[room].answers[q] = [];
-    rooms[room].answers[q].push(player);
-    // Quand tout le monde a répondu, on passe à la suite
-    if (rooms[room].answers[q].length === rooms[room].players.length) {
-      rooms[room].index++;
-      if (rooms[room].index < sampleQuestions.length) {
-        io.to(room).emit('nextQuestion', rooms[room].index);
+    const roomData = rooms[room];
+    if (!roomData) return;
+
+    if (!roomData.answers[questionIndex]) {
+      roomData.answers[questionIndex] = [];
+    }
+
+    roomData.answers[questionIndex].push(player);
+
+    const allAnswered = roomData.answers[questionIndex].length === roomData.players.length;
+
+    if (allAnswered) {
+      roomData.index++;
+      if (roomData.index < sampleQuestions.length) {
+        io.to(room).emit('nextQuestion', roomData.index);
       } else {
-        io.to(room).emit('gameOver', rooms[room].answers);
+        io.to(room).emit('gameOver', roomData.answers);
       }
     }
   });
 });
 
-const sampleQuestions = [
-  "Qui adore les chats ?",
-  "Qui choisirait toujours du bleu ?",
-  "Qui pourrait jouer dans un film d'action ?"
-];
-
-server.listen(3001, () => {
-  console.log('Server running on port 3001');
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Serveur lancé sur le port ${PORT}`);
 });
